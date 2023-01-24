443ab2474c0617bdc9c4e0d17e2eb39b7b159082
roshan-bajgain <roshan.bazgain@gmail.com>
2023-01-24 16:29:00 +0545

    Add component and routes

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
 import './App.css';
+import Header from './container/Header';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import ProductListing from './container/ProductListing';
+import ProductDetail from './container/ProductDetail';
 
 function App() {
   return (
     <div className="App">
-       <h1>Hello World</h1>
+      <Router>
+        <Header />
+        <Routes >
+          <Route exact path="/" element={<ProductListing />} />
+          <Route exact path="/product/:productId" element={<ProductDetail />} />
+          <Route>404 not found</Route>
+        </Routes >
+      </Router>
+       
     </div>
   );
 }
---
+++ src/container/ProductListing.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const ProductListing = () => {
+  return (
+    <div>
+        <h1>Product listing...</h1>
+    </div>
+  )
+}
+
+export default ProductListing
