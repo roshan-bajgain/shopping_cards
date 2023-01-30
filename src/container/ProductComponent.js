97bab69942ea8927804301e7994ff7b32958afa8
roshan-bajgain <roshan.bazgain@gmail.com>
2023-01-30 13:24:36 -0800

    Destructure the element from array and show the title

--- src/container/ProductComponent.js
+++ src/container/ProductComponent.js
@@ -1,9 +1,21 @@
-import React from 'react'
+import React from 'react';
+import { useSelector } from 'react-redux';
 
 const ProductComponent = () => {
+  const product = useSelector((state) => state.allproducts.product);
+  const{ id,title } = product[0];
   return (
-    <div>ProductComponent</div>
+    <div className="four column wide">
+      <div className="ui link cards">
+        <div className="card">
+          <div className="image"></div>
+          <div className="content">
+            <div className="header">{title}</div>
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
 
-export default ProductComponent
+export default ProductComponent;
--- src/container/ProductListing.js
+++ src/container/ProductListing.js
@@ -1,9 +1,13 @@
 import React from 'react'
+import ProductComponent from './ProductComponent';
+import { useSelector } from 'react-redux'
 
 const ProductListing = () => {
+  const product = useSelector((state) => state);
+  console.log(product);
   return (
-    <div>
-        <h1>Product listing...</h1>
+    <div className="ui grid container">
+        <ProductComponent />
     </div>
   )
 }
